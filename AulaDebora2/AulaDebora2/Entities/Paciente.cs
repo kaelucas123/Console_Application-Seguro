f4c0a42394179ef93b62bf3ac8ee9977afa4a2c3
Kae <80605454+kaelucas123@users.noreply.github.com>
2022-08-31 12:18:14 -0300

    Commit inicial

---
+++ AulaDebora2/AulaDebora2/Entities/Paciente.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+
+
+namespace AulaDebora2.Entities
+{
+    internal class Paciente
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+        public int numeroDeDependentes { get; set; }
+        public string endereço { get; set; }
+        public string cep { get; set; }
+        public string estado { get; set; }
+        public int tempoDeEmpresa { get; set; }
+        public double salario { get; set; }
+        public bool doencas { get; set; }
+        public string infoEs, infoTe, infoSa, InfoDo;
+
+        StringBuilder sb = new StringBuilder();
+        
+        public Paciente(string nome, int idade,int numeroDeDependentes, string endereço, string cep, string estado, int tempoDeEmpresa, double salario, bool doencas)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.endereço = endereço;
+            this.cep = cep;
+            this.estado = estado;
+            this.tempoDeEmpresa = tempoDeEmpresa;
+            this.salario = salario;
+            this.doencas = doencas;
+            this.numeroDeDependentes = numeroDeDependentes;
+        }
+        
+        public Paciente()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            if (estado != "SP")
+            {
+                infoEs = "Não oferecemos cobertura para outros estados.";
+            }
+            else
+            {
+                infoEs = "Area dentro de cobertura.";
+            }
+            if(tempoDeEmpresa < 2)
+            {
+                infoTe = "Infelizmente seu tempo é menor que 2 anos e não oferecemos cobertura.";
+            }
+            else
+            {
+                infoTe = "Cobertura disponível.";
+            }
+            if(salario < 1500)
+            {
+                infoSa = "Salário insuficiente ";
+            }
+            if(1500 < salario && salario< 3000)
+            {
+                infoSa = "Cliente Gold";
+            }
+            else
+            {
+                infoSa = "Cliente Platinum";
+            }
+            if(doencas == true)
+            {
+                InfoDo = "Sujeito à análise";
+            }
+            else
+            {
+                InfoDo = "Análise não necessária";
+            }
+            sb.AppendLine("--------------------------------------------------------------------------------------------------------------------");
+            sb.AppendLine("Informações do paciente: ");
+            sb.Append($"| Nome: {nome} | Idade: {idade} | Número de dependentes: {numeroDeDependentes} | Endereço: {endereço} | Cep: {cep} | Estado: {estado}\n| Tempo de empresa: {tempoDeEmpresa} | Salario: {salario} | Doenças pré-existentes: {doencas}\n");
+            sb.AppendLine("--------------------------------------------------------------------------------------------------------------------");
+            sb.AppendLine("Informações de serviços:");
+            sb.Append($"| Info estado: {infoEs} |\n| Info tempo de empresa: {infoTe} |\n| Info salario: {infoSa} |\n| Info doenças pré-existentes: {InfoDo} |\n");
+                       
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ AulaDebora2/AulaDebora2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using AulaDebora2.Entities;
+
+namespace AulaDebora2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Paciente paciente = new Paciente();
+            Console.Write("Entre seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Entre sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            Console.Write("Número de dependentes: ");
+            int numeroDeDependentes = int.Parse(Console.ReadLine());
+            Console.Write("Entre seu endereço: ");
+            string endereco = Console.ReadLine();
+            Console.Write("Entre seu CEP: ");
+            string cep = Console.ReadLine();
+            Console.Write("Entre com seu estado: ");
+            string estado = Console.ReadLine();
+            Console.Write("Entre seu tempo de empresa: ");
+            int tempoDeEmpresa = int.Parse(Console.ReadLine());
+            Console.Write("Entre seu Salário: ");
+            double salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("Tem doenças pré-existentes?");
+            Console.WriteLine("(1) Sim\n(2) Não");
+            bool doenca = false;
+            int resp = int.Parse(Console.ReadLine());
+
+            if(resp == 1)
+            {
+                doenca = true;
+            }
+            else
+            {
+                doenca = false;
+            }
+
+            paciente = new Paciente(nome, idade, numeroDeDependentes, endereco, cep, estado.ToUpper(), tempoDeEmpresa, salario, doenca);
+
+            Console.WriteLine(paciente);
+
+        }
+    }
+}
